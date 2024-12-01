cc4cda51192624a7215a11cf7b7981ec2734a801
Piyush Tiwari <143063765+PiyushTiwari10@users.noreply.github.com>
2024-12-01 10:18:53 +0530

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "../style"; // Assuming styles are imported from your styles file
 
 const Button = ({ additionalStyles }) => (
-  <a href="https://lexi-bot.streamlit.app/" target="_blank" rel="noopener noreferrer">
+  <a href="https://image-processing-chatbot-rugnpnc4qfzrvxmtydzkrj.streamlit.app/" target="_blank" rel="noopener noreferrer">
     <button
       type="button"
       className={`py-4 px-6 my-6 font-poppins font-bold text-[18px] text-black bg-gradient-button rounded-[10px] outline-none ${additionalStyles}`}
