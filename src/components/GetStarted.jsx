aa4fe00c32a2b1dbf16121866f918334f1ffb7e0
Piyush Tiwari <143063765+PiyushTiwari10@users.noreply.github.com>
2025-01-12 18:39:54 +0530

    Update GetStarted.jsx

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -3,7 +3,7 @@ import styles from "../style";
 import { arrowUp } from "../assets";
 
 const GetStarted = () => (
-  <a href="https://image-processing-chatbot-rugnpnc4qfzrvxmtydzkrj.streamlit.app/" target="_blank" rel="noopener noreferrer">
+  <a href="https://lexi-bot.streamlit.app/" target="_blank" rel="noopener noreferrer">
     <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
       <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
         <div className={`${styles.flexStart} flex-row`}>
